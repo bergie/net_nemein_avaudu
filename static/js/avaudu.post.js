c97919d28c20ae87c3d62bec60d7696bb0e8d721
Joonas Bergius <joonas.bergius@gmail.com>
2009-06-05 19:30:44 +0300

    Fixed the response prepending javascript

--- static/js/avaudu.post.js
+++ static/js/avaudu.post.js
@@ -7,13 +7,13 @@
             if ($message)
             {
                 $.post('/statuses/update.json',{
-                 status: $message},
-                 function(json){
+                status: $message},
+                function(json){
                     $input.blur();
                     $activity = $('#main #activity');
                     if ($activity.size() > 0)
                     {
-                        $activity.prepend('<li class="" id="'+json.id+'"><div class="profile"><img src="'+json.user.profile_image_url+'" /></div><div class="content"><p class="author">'+json.user.screen_name+'</p><div class="entry">'+json.text+'</div></div></li>');
+                        $activity.prepend('<li class="" id="'+json[0].id+'"><div class="profile"><img src="'+json[0].user.profile_image_url+'" /></div><div class="content"><p class="author">'+json[0].user.screen_name+'</p><div class="entry">'+json[0].text+'</div></div></li>');
                     }
                 },
                 "json");
