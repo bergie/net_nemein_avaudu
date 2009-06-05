c593f1fd03ed48d67415800746fd938abb18490b
Joonas Bergius <joonas.bergius@gmail.com>
2009-06-05 19:00:39 +0300

    Post our own reply to the top of the feed

--- static/js/avaudu.post.js
+++ static/js/avaudu.post.js
@@ -8,9 +8,15 @@
             {
                 $.post('/statuses/update.json',{
                  status: $message},
-                 function(){
+                 function(json){
                     $input.blur();
-                });
+                    $activity = $('#main #activity');
+                    if ($activity.size() > 0)
+                    {
+                        $activity.prepend('<li class="" id="'+json.id+'"><div class="profile"><img src="'+json.user.profile_image_url+'" /></div><div class="content"><p class="author">'+json.user.screen_name+'</p><div class="entry">'+json.text+'</div></div></li>');
+                    }
+                },
+                "json");
             }
             return false;
         });
